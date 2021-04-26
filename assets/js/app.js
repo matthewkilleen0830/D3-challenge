bcf5c00220946a29915eaa1d1c634c8d2cc5d7cc
matthewkilleen0830 <matthew.killeen0830@gmail.com>
2021-04-26 16:52:46 -0500

    Added axis labels.

--- assets/js/app.js
+++ assets/js/app.js
@@ -65,7 +65,7 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
         .attr("opacity", "1.0");
 
     // Create another variable to display state abbreviations on circles
-    var textGroup = chartGroup.selectAll("null")
+    var textGroup = chartGroup.selectAll(null)
         .data(censusData)
         .enter()
         .append("text")
@@ -94,4 +94,16 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
             toolTip.hide(data);
         });
 
+    // Create axis labels
+    chartGroup.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left + 40)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .attr("class", "axisText")
+        .text("Lacks Healthcare (%)");
+    chartGroup.append("text")
+        .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+        .attr("class", "axisText")
+        .text("In Poverty (%)");
 });
