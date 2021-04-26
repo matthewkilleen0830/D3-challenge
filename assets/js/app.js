cf6043e600c2c735ed23c0c7759a86fa0312d306
matthewkilleen0830 <matthew.killeen0830@gmail.com>
2021-04-26 16:32:05 -0500

    Created scatter plot circles and tool tip.

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,7 @@ var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper
-var svg = d3.select(".scatter")
+var svg = d3.select("#scatter")
     .append("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
@@ -36,10 +36,10 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
 
     // Create linear scale functions for x axis and y axis
     var xLinearScale = d3.scaleLinear()
-        .domain([20, d3.max(censusData, d => d.poverty)])
+        .domain([d3.min(censusData, d => d.poverty) - 1, d3.max(censusData, d => d.poverty) + 1])
         .range([0, width]);
     var yLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(censusData, d => d.healthcare)])
+        .domain([d3.min(censusData, d => d.healthcare) - 1, d3.max(censusData, d => d.healthcare) + 1])
         .range([height, 0]);
 
     // Create axis functions
@@ -54,6 +54,36 @@ d3.csv("assets/data/data.csv").then(function(censusData) {
         .call(leftAxis);
     
     // Create scatter plot circles
+    var circlesGroup = chartGroup.selectAll("circle")
+        .data(censusData)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xLinearScale(d.poverty))
+        .attr("cy", d => yLinearScale(d.healthcare))
+        .attr("r", "15")
+        .attr("fill", "#8abcd5")
+        .attr("opacity", "1.0");
 
+    // Create another variable to display state abbreviations on circles
+    var textGroup = chartGroup.selectAll("null")
+        .data(censusData)
+        .enter()
+        .append("text")
+        .text(d => d.abbr)
+        .attr("x", d => xLinearScale(d.poverty))
+        .attr("y", d => yLinearScale(d.healthcare))
+        .attr("dx", d => -9)
+        .attr("dy", d => 6);
+        
+    // Initialize tool tip
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(function(d) {
+        return (`${d.state}<br>In Poverty: ${d.poverty}<br>Lacks Healthcare: ${d.healthcare}`);
+        });
+
+    // Create tool tip in the plot
+    chartGroup.call(toolTip);
 
 });
